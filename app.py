79155d763c43d06cc4199e651794a4d0db4f6528
ElizavettaMak <makyhinaeliza@gmail.com>
2025-04-23 21:39:54 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,60 +1,58 @@
-def calculator_app (a: str, b: str) -> None:
-    """Функция вычисления результата между двумя числами
+def check_number (a: str) -> None:
+    """Функция проверки числа, вводимого пользователем
 
-    :param a: "Введите число №1: ".
-    :param b: "Введите число №2: ".
+    :param a: "Введите число №: ".
     :return: None
     """
 
-    print("Добро пожаловать в приложение 'Калькулятор'!")
     while True:
-        while True: # проверка числа №1
+        try:
+            number_1_ch = input(a)
+            number_1 = float(number_1_ch)
+            if number_1.is_integer():
+                number_1 = int(number_1_ch)
+            return number_1
+            break
+        except ValueError:
+            print("Ошибка! Введите число.")
+            continue
+
+def check_operation(number_1: int|float, number_2: int|float) -> int|float:
+    """Функция проверки операции, выбранной пользователем
+
+    :param a: Число №1.
+    :param b: Число №2.
+    :return: Результат вычисления
+    """
+
+    while True:
+        operation = input("Введите операцию из списка (+, -, *, /): ")
+        if operation == '+':
+            result = number_1 + number_2
+            break
+        elif operation == '-':
+            result = number_1 - number_2
+            break
+        elif operation == '*':
+            result = number_1 * number_2
+            break
+        elif operation == '/':
             try:
-                number_1_ch = input(a)
-                number_1 = float(number_1_ch)
-                if number_1.is_integer():
-                    number_1 = int(number_1_ch)
+                result = number_1 / number_2
                 break
-            except ValueError:
-                print("Ошибка! Введите число.")
+            except ZeroDivisionError:
+                print("Деление на ноль запрещено!")
                 continue
+        else:
+            print("В списке такой операции нет.")
+            continue
+    return result
 
-        while True: # проверка числа №2
-            try:
-                number_2_ch = input(b)
-                number_2 = float(number_2_ch)
-                if number_2.is_integer():
-                    number_2 = int(number_2_ch)
-                break
-            except ValueError:
-                print("Ошибка! Введите число.")
-                continue
+def calculator_app () -> None:
+    print("Добро пожаловать в приложение 'Калькулятор'!")
+    val1 = check_number('Введите число №1: ')
+    val2 = check_number('Введите число №2: ')
+    result = check_operation(val1, val2)
+    print("Результат:", result)
 
-        while True: # выбор операции
-            operation = input("Введите операцию из списка (+, -, *, /): ")
-            if operation == '+':
-                result = number_1 + number_2
-                print("Результат:", result)
-                break
-            elif operation == '-':
-                result = number_1 - number_2
-                print("Результат:", result)
-                break
-            elif operation == '*':
-                result = number_1 * number_2
-                print("Результат:", result)
-                break
-            elif operation == '/':
-                try:
-                    result = number_1 / number_2
-                    print("Результат:", result)
-                    break
-                except ZeroDivisionError:
-                    print("Деление на ноль запрещено!")
-                    continue
-            else:
-                print("В списке такой операции нет.")
-                continue
-        break
-
-calculator_app("Введите число №1: ", "Введите число №2: ")
+calculator_app ()
